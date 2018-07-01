86596b9a0c6641bfd938c0498de4ef9f4ae42178
Simen Gaure <simen@gaure.no>
2018-07-01 18:26:11 +0200

    tidy

--- R/ipol.R
+++ R/ipol.R
@@ -4,13 +4,16 @@ ipol <- function(val,dims=NULL,intervals=NULL,grid=NULL,knots=NULL,k=NULL,
   method <- match.arg(method)
   switch(method,
          chebyshev={
+           if(is.function(val) && is.null(dims)) stop('dims must be specified')
            if(is.function(val)) return(chebappxf(val,dims,intervals,...))
            return(chebappx(val,intervals))
          },
          multilinear={
+           if(is.null(grid)) stop('grid must be specified for multi linear interpolation')
            return(mlappx(val,grid,...))
          },
          fh={
+           if(is.null(grid)) stop('grid must be specified for Floater-Hormann interpolation')
            if(is.null(k)) k <- min(4,sapply(grid,length))
            return(fhappx(val,grid,d=k))
          },
@@ -20,6 +23,7 @@ ipol <- function(val,dims=NULL,intervals=NULL,grid=NULL,knots=NULL,k=NULL,
            return(ucappx(val,intervals))
          },
          general={
+           if(is.null(grid)) stop('grid must be specified for general interpolation')
            if(is.function(val)) return(chebappxgf(val,grid,...))
            return(chebappxg(val,grid))
          },
--- tests/all.R
+++ tests/all.R
@@ -15,11 +15,11 @@ cat('Long test:',ch(0.03),f(0.03),ch(0.031),f(0.031),'\n')
 f <- function(x) exp(-sum(x^2))
 
 dims <- c(8,7,6)
-ch <- chebappxf(f,dims)
+ch <- ipol(f,dims=dims,method='cheb')
 s <- runif(3,-1,1)
 ch(s)-f(s)
 iv <- list(c(1,2),c(1,4),c(1,3))
-ch <- chebappxf(f,dims,iv)
+ch <- ipol(f,dims=dims,intervals=iv)
 s <- c(1.4,2.3,1.9)
 ch(s) - f(s)
 a <- c(1.01,1.01,1.01) ; ch(a)- f(a)
@@ -40,7 +40,8 @@ su <- seq(0,1,length.out=10)
 ## irregularly spaced grid-points
 s <- su^3
 ## create approximation on the irregularly spaced grid
-ch <- Vectorize(chebappxg(exp(s),list(s)))
+ch <- ipol(exp(s),grid=list(s), method='general')
+#ch <- Vectorize(chebappxg(exp(s),list(s)))
 ## test it:
 r <- runif(1); cat('true:',exp(r),'appx:',ch(r),'\n')
 
@@ -50,13 +51,13 @@ grid <- list(su,su^2,su^3)
 dims <- lapply(grid,length)
 
 fv <- structure(apply(expand.grid(grid),1,f),dim=lapply(grid,length))
-ch <- chebappxg(fv,grid)
+ch <- ipol(fv,grid=grid,method='general')
 s <- runif(3)
 cat('true:',f(s),'appx:',ch(s),'\n')
 
 # multi linear
 s <- runif(3)
-lip <- mlappx(fv,grid)
+lip <- ipol(fv,grid=grid,method='multi')
 cat('true',f(s), 'appx:', ch(s), 'lip:',lip(s),'\n')
 
 # test dct transform
@@ -68,21 +69,21 @@ chebcoef(a,TRUE)
 f <- function(x) 1/(1+25*x^2)
 grid <- seq(-1,1,length.out=15)
 val <- f(grid)
-uc <-ucappx(val)
+uc <- ipol(val,method='uniform')
 # and the Chebyshev
-ch <- chebappxf(f,15)
+ch <- ipol(val,dims=15,method='cheb')
 # test it at 10 random points
 a <- runif(10)
 cbind(uc(a),ch(a),sapply(a,f))
 
-uc <- ucappxf(f,15,intervals=c(-1,1))
+uc <- ipol(f,dims=15,intervals=c(-1,1),method='uniform')
 a <- runif(1,-1,1)
 uc(a); ch(a); f(a)
 
 #polyharmonic splines
 f <- function(x) 10/(10+sum(sqrt(x)))
 knots <- matrix(runif(6000,0,20), 6)
-phs <- polyh(f, knots, 3)
+phs <- ipol(f,knots=knots,k=3,method='poly')
 round(fivenum(phs(knots)-apply(knots,2,f)),6)
 # test it in a random point
 a <- runif(6,0,20)
@@ -97,12 +98,10 @@ phs(a); nphs(a)
 
 f <- function(x) sum(log(1+x^2))
 grid <- list(seq(-1,1,len=50), seq(-1,1,len=30)^3L, chebknots(20)[[1]])
-fh <- fhappx(f,grid=grid)
+fh <- ipol(f,grid=grid,method='fh')
 x <- matrix(runif(3*1e2),3)
 fh.v <- fh(x,2)
 f.v <- apply(x,2,f)
 round(fivenum(fh.v-f.v),7)
 # check some points:
 round(f(sapply(grid,function(x) x[4])) - fh(sapply(grid,function(x) x[4]+1e-9)),7)
-
-
