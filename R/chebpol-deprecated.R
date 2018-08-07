58ab732932287349279a42f9a7895866f9fccedf
Simen Gaure <simen@gaure.no>
2018-08-07 20:23:44 +0200

    made all the specific functions deprecated

---
+++ R/chebpol-deprecated.R
@@ -0,0 +1,9 @@
+#' Deprecated functions
+#'
+#' As of version 1.7, the functions for creating various interpolants
+#' have been deprecated. These are \code{chebappx, chebappxf, chebappxg,
+#' chebappxgf, fhappx, mlappx, slappx, ucappx, ucappxf, polyh}. The wrapper \code{ipol}
+#' should be used instead. The wrapper gives a uniform interface.
+#' @name chebpol-deprecated
+#' @aliases chebpol-deprecated
+NULL
